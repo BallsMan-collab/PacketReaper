376d6d6e01532dff4d9db31ac862d1cf42c006d9
Not Anonymous <dia17461@diaestudents.com>
2025-03-28 15:27:18 +0400

    Create banner.py

---
+++ utils/banner.py
@@ -0,0 +1,25 @@
+# utils/banner.py
+
+def print_banner():
+    """
+    Prints the ASCII art banner for PacketReaper.
+    """
+    banner = r"""
+888b    888          888                d8888                                                                              
+8888b   888          888               d88888                                                                              
+88888b  888          888              d88P888                                                                              
+888Y88b 888  .d88b.  888888          d88P 888 88888b.   .d88b.  88888b.  888  888 88888b.d88b.   .d88b.  888  888 .d8888b  
+888 Y88b888 d88""88b 888            d88P  888 888 "88b d88""88b 888 "88b 888  888 888 "888 "88b d88""88b 888  888 88K      
+888  Y88888 888  888 888           d88P   888 888  888 888  888 888  888 888  888 888  888  888 888  888 888  888 "Y8888b. 
+888   Y8888 Y88..88P Y88b.        d8888888888 888  888 Y88..88P 888  888 Y88b 888 888  888  888 Y88..88P Y88b 888      X88 
+888    Y888  "Y88P"   "Y888      d88P     888 888  888  "Y88P"  888  888  "Y88888 888  888  888  "Y88P"   "Y88888  88888P' 
+                                                                              888                                          
+                                                                         Y8b d88P                                          
+                                                                          "Y88P"                                           
+          The Ultimate Wi-Fi Pentesting Tool
+"""
+    print(banner)
+
+if __name__ == "__main__":
+    # Example usage (for testing the module directly)
+    print_banner()
